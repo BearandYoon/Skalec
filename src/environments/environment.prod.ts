fa742ee20826ba6b64a27e9ea1125e626422ff4c
BearandYoon <luckysevenbear@gmail.com>
2018-01-23 04:14:25 +0100

    add firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDSW3ypSxLFrTl2aL9Bx6NZ2U1CQSVcZdg',
-    authDomain: 'framework-dev.firebaseapp.com',
-    databaseURL: 'https://framework-dev.firebaseio.com',
-    projectId: 'framework-dev',
-    storageBucket: 'framework-dev.appspot.com',
-    messagingSenderId: '1084724587881'
+    apiKey: 'AIzaSyAtBlLB2ilOBj8A0zxx75BFWW55el4jZpo',
+    authDomain: 'skalec-c3f37.firebaseapp.com',
+    databaseURL: 'https://skalec-c3f37.firebaseio.com',
+    projectId: 'skalec-c3f37',
+    storageBucket: 'skalec-c3f37.appspot.com',
+    messagingSenderId: '1056907042243'
   }
 };
